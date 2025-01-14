2461357b0888b22871a282b5d8e9d01daba82e55
Varshini <varshiniprabakaran0506@gmail.com>
2025-01-14 21:46:31 +0530

    added participation routes

--- backend/index.js
+++ backend/index.js
@@ -1,76 +1,76 @@
 const express = require("express")
-const {PrismaClient} = require("@prisma/client")
+const { PrismaClient } = require("@prisma/client")
 const bcrypt = require("bcrypt")
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: 'ds65kgmhq',
-    api_key: '614127347772552',
-    api_secret: 'xAG6lDg6nUKIhJAS9BXuKqPq2tA',
-  });
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'hackathons', 
-      resource_type: 'auto', 
-    },
-  });
+  cloudinary: cloudinary,
+  params: {
+    folder: 'hackathons',
+    resource_type: 'auto',
+  },
+});
 
-const app= express()
+const app = express()
 var prisma = new PrismaClient();
 const upload = multer({ storage });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/signin",async(req,res)=>{
-  const {email , password, role}= req.body;
-  try{
-   const isuserexist = await prisma.user.findUnique({
-    where:{
+app.post("/signin", async (req, res) => {
+  const { email, password, role } = req.body;
+  try {
+    const isuserexist = await prisma.user.findUnique({
+      where: {
         email: email
-    },
-   })
-   if(isuserexist){
-    return res.status(409).json({message:"User already exists"})
-   }
-   const hashedpassword = await bcrypt.hash(password,10);
-   const newuser = await prisma.user.create({
-    data:{
-       email: email,
-       password: hashedpassword,
-       role: role
-    },
-   })
-   return res.status(201).json({message:"User created successfully",data:newuser})
-  }catch(error){  
-    return res.status(500).json({message:error})
+      },
+    })
+    if (isuserexist) {
+      return res.status(409).json({ message: "User already exists" })
+    }
+    const hashedpassword = await bcrypt.hash(password, 10);
+    const newuser = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashedpassword,
+        role: role
+      },
+    })
+    return res.status(201).json({ message: "User created successfully", data: newuser })
+  } catch (error) {
+    return res.status(500).json({ message: error })
   }
 })
 
-app.post("/login",async(req,res)=>{
-  const {email,password}= req.body;
-  try{
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
     const isUserExists = await prisma.user.findUnique({
-      where:{ email },
+      where: { email },
     });
 
-    if(!isUserExists){
+    if (!isUserExists) {
       return res
-      .status(404)
-      .json({ message: "user email not exists.Sign up" });
+        .status(404)
+        .json({ message: "user email not exists.Sign up" });
     }
     const passwordmatch = await bcrypt.compare(password, isUserExists.password);
 
     if (!passwordmatch) {
       return res.status(401).json({ message: "Invalid user password" });
-    }else{
-       return res.status(201).json({message:"login successfull",data: isUserExists});
+    } else {
+      return res.status(201).json({ message: "login successfull", data: isUserExists });
     }
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error." });
   }
@@ -104,37 +104,52 @@ app.post("/getprofile", async (req, res) => {
   }
 });
 
-app.post("/participation", upload.fields([{ name: 'image', maxCount: 5 }, { name: 'pdf', maxCount: 1 }]), async (req,res)=>{
-    const data =  req.body;
-    console.log(req.files);
-
-    try{
-        if (!data.user_id || !data.competition_name || !data.college || !data.date){
-            return res.status(400).json({message:"All Fields are required"});
-        }
-        
-        const imageUrls = req.files['image'] 
-          ? req.files['image'].map(file => file.path).join(', ') 
-          : '';
-        const pdfUrl = req.files['pdf'] ? req.files['pdf'][0].path : null; 
-        
-        const participation = await prisma.participation.create({
-            data:{
-              user_id: parseInt(data.user_id), 
-                competition_name: data.competition_name,
-                college:data.college,
-                date : data.date,
-                certificates : imageUrls,
-                report : pdfUrl
-            },
-        });
-        return res.status(201).json({message:"Participation details successfully stored", data:participation});
-    } catch(error){
-        return res.status(500).json({message:error})
+app.post("/participation", upload.fields([{ name: 'image', maxCount: 5 }, { name: 'pdf', maxCount: 1 }]), async (req, res) => {
+  const data = req.body;
+  
+  try {
+    if (!data.user_id || !data.competition_name || !data.college || !data.date) {
+      return res.status(400).json({ message: "All Fields are required" });
     }
+
+    const imageUrls = req.files['image']
+      ? req.files['image'].map(file => file.path).join(', ')
+      : '';
+    const pdfUrl = req.files['pdf'] && req.files['pdf'][0] ? req.files['pdf'][0].path : null;
+
+    const participation = await prisma.participation.create({
+      data: {
+        user_id: parseInt(data.user_id),
+        competition_name: data.competition_name,
+        college: data.college,
+        date: data.date,
+        certificates: imageUrls,
+        report: pdfUrl
+      },
+    });
+    return res.status(201).json({ message: "Participation details successfully stored", data: participation });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
+app.get("/getparticipation/:user_id",async (req,res)=>{
+  const {user_id} = req.params;
+  console.log(user_id);
+
+  try{
+    const participationdata = await prisma.participation.findMany({
+      where:{
+        user_id : parseInt(user_id)
+      }
+    });
+    return res.status(200).json({message:"Data retrieved successfully",data:participationdata});
+  }catch(error){
+    return res.status(500).json({message:error});
+  }
+  
 });
 
 app.listen(3005, () => {
-    console.log("Server is running on port 3002");
-  });
-  
+  console.log("Server is running on port 3005");
+});
