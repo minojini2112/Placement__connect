acd5ef7d92fc49b03671952a9d67ad13476232e1
Minojini <minojini05@gmail.com>
2025-01-15 20:12:01 +0530

    added sidemenu

--- Frontend/src/Components/Student/Profile/Profile.jsx
+++ Frontend/src/Components/Student/Profile/Profile.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div>
+      <div>
+        <div>
+          <h2>name</h2>
+          <h3>year</h3>
+          <h3>department</h3>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
