eec7452515ba643633111d1d5e77a2292d6eabbb
Varshini <varshiniprabakaran0506@gmail.com>
2025-01-15 14:03:26 +0530

    Added router in frontend

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom/client";
-const Para=()=>{
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import Dashboard from "./Components/Student/Dashboard/Dashboard";
+import Profile from "./Components/Student/Profile/Profile";
+import Participation from "./Components/Student/Participation/Participation";
+import Notification from "./Components/Student/Notifications/Notifications";
+import Sidemenu from "./Components/Sidemenu";
+
+const App=()=>{
     return(
-        <h1>hello</h1>
+        <div className="flex">
+        <Sidemenu/>
+        <Outlet/>
+        </div>
     );
-}
+};
+
+const Router = createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+        children:[
+            {
+                path:"/",
+                element:<Dashboard/>,
+            },
+            {
+                path:"/profile",
+                element:<Profile/>,
+            },
+            {
+                path:"/participation",
+                element:<Participation/>,
+            },
+            {
+                path:"notifications",
+                element:<Notification/>,
+            }
+        ]
+    }
+]);
+
 const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Para/>)
+root.render(<RouterProvider router = {Router}/>);
